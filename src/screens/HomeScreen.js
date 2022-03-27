a01dd648d03d2d18927a5019495678ec665b3087
VinhKyIT <vinhkyit2905@gmail.com>
2022-03-27 23:53:54 +0700

    init redux, first thing of home screen and more...

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { SafeAreaView, Text, View, Image } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png',
+          }}
+          style={{ width: 100, height: 100 }}
+          resizeMode="contain"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
