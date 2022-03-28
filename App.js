ff43099e799e1d06bab67b471f1fbf3a4cdba5ef
VinhKyIT <vinhkyit2905@gmail.com>
2022-03-28 21:59:04 +0700

    nav options done and install rn navigation deps

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import HomeScreen from './src/screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 };
--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -1,6 +1,7 @@
 import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
 
 const data = [
   {
@@ -35,6 +36,12 @@ const NavOptions = () => {
                 resizeMode="contain"
               />
               <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+              <Icon
+                style={tw` p-2 bg-black rounded-full w-10 mt-4`}
+                name="arrowright"
+                color="white"
+                type="antdesign"
+              />
             </View>
           </TouchableOpacity>
         );
