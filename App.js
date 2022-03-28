cc3ebcf2d43c38cf5d341d8f65d1c4836cc6dd26
VinhKyIT <vinhkyit2905@gmail.com>
2022-03-28 22:48:28 +0700

    implement react native stack navigator

--- App.js
+++ App.js
@@ -4,13 +4,25 @@ import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import HomeScreen from './src/screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './src/screens/MapScreen';
+import EatsScreen from './src/screens/EatsScreen';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Map" component={MapScreen} />
+            <Stack.Screen name="Eats" component={EatsScreen} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 };
--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -2,6 +2,7 @@ import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
   {
@@ -9,18 +10,19 @@ const data = [
     title: 'Get a ride',
     image:
       'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png',
-    screen: 'MapScreen',
+    screen: 'Map',
   },
   {
     id: 2,
     title: 'Order food',
     image:
       'https://i.pinimg.com/originals/4f/eb/74/4feb745209cf7aba57463b20d27b61e3.png',
-    screen: 'EatsScreen',
+    screen: 'Eats',
   },
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
     <FlatList
       data={data}
@@ -28,7 +30,9 @@ const NavOptions = () => {
       keyExtractor={item => item.id}
       renderItem={({ item }) => {
         return (
-          <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <TouchableOpacity
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+            onPress={() => navigation.navigate(item.screen)}>
             <View>
               <Image
                 source={{ uri: item.image }}
