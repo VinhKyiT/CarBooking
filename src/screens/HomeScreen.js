030418daceba14e728164ce39ece5db072947135
VinhKyIT <vinhkyit2905@gmail.com>
2022-04-01 23:44:48 +0700

    done auto complete places

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_API_KEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setOrigin, setDestination } from '../redux/slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -18,8 +22,33 @@ const HomeScreen = () => {
         />
         <GooglePlacesAutocomplete
           placeholder="Search"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              }),
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          minLength={2}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
+          query={{
+            key: GOOGLE_API_KEY,
+            language: 'vi',
+          }}
         />
         <NavOptions />
       </View>
